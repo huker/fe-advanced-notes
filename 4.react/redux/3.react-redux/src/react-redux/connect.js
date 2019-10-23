3b78af050bfa64d2d7ee510676e966b193502946
huker <huaolie@sina.com>
2019-10-23 23:05:28 +0800

    react-redux render

--- 4.react/redux/3.react-redux/src/react-redux/connect.js
+++ 4.react/redux/3.react-redux/src/react-redux/connect.js
@@ -11,12 +11,12 @@ import { bindActionCreators } from '../redux';
 export default function (mapStateToProps, mapDispatchToProps) {
     return function (InnerComponent) {
         return class extends React.Component {
-            // 累的静态属性 this.context = {store:xxx}
+            // 类的静态属性 this.context = {store:xxx}
             static contextType = Context;
 
             constructor(props, context) {
                 super(props);
-                this.state = mapStateToProps(context.store.getState());
+                this.state = this.mapState = mapStateToProps(context.store.getState());
                 /**
                  * mapDispatchToProps
                  * 有可能是个函数 也有可能是个对象 所以要判断下
@@ -29,9 +29,30 @@ export default function (mapStateToProps, mapDispatchToProps) {
                 }
             }
 
+            /**
+             * update时当前的state和新的mapStateToProps返回的state是否一致
+             * 优化渲染的 方法一
+             */
+            // shouldComponentUpdate() {
+            //     // 这边this.state一直是初始的状态 所以永远是不一样的 要重新赋值一个对象来记录老state
+            //     let nextState = mapStateToProps(this.context.store.getState());
+            //     if (this.mapState === nextState) {
+            //         return false
+            //     }
+            //     this.mapState = nextState
+            //     return true
+            // }
+
+            /**
+             * 优化渲染方式2 可以直接在这边做完 不去考虑shouldupdate
+             */
             componentDidMount() {
                 this.unsubscribe = this.context.store.subscribe(() => {
-                    this.setState(mapStateToProps(this.context.store.getState()))
+                    let nextState = mapStateToProps(this.context.store.getState());
+                    if (this.mapState !== nextState) {
+                        this.mapState = nextState;
+                        this.setState(nextState);
+                    }
                 })
             }
 
--- 4.react/redux/3.react-redux/src/redux/combineReducers.js
+++ 4.react/redux/3.react-redux/src/redux/combineReducers.js
@@ -3,12 +3,25 @@
  */
 
 export  default function combineReducers(reducers) {
-    //返回的还是一个reducer函数
+    //返回的还是一个reducer函数 返回的是新的状态
     return function (state = {}, action) {
+        // 这样做指针就每次都变了 会导致没有变化的组件也重新渲染 所以要稍加优化
+        // let nextState = {};
+        // for ( let key in reducers ) {
+        //     nextState[key] = reducers[key](state[key], action)
+        // }
+
         let nextState = {};
+        let hasChanged = false;
         for ( let key in reducers ) {
-            nextState[key] = reducers[key](state[key], action)
+            let reducer = reducers[key];
+            // 老状态
+            let previousState = state[key];
+            let newState = reducer(previousState, action);
+            nextState[key] = newState;
+            hasChanged = hasChanged || (newState != previousState);
         }
-        return nextState
+        // 有一个state改变就返回新的state
+        return hasChanged ? nextState : state
     }
 }
